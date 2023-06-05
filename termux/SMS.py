35474367d123316c2030afc8b12980d19d744d6e
Dr Shajul <drshajul@gmail.com>
2023-06-05 23:51:38 +0530

    Bugfix

--- termux/SMS.py
+++ termux/SMS.py
@@ -38,8 +38,8 @@ def send(text, number, sim: int = None):
   if sim is not None:
     opts += ['-s', sim]
   if type(number) is list:
-    opts.append("-n", ",".join(number))
+    opts += ["-n", ",".join(number)]
   elif type(number) is str:
-    opts.append("-n", number)
+    opts += ["-n", number]
   opts.append(text)
   return execute(["termux-sms-send"] + opts)
